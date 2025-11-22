30a8f428b28f1aa01055bd57f7b2257b465844fe
vais2004 <vkawale2004@gmail.com>
2025-11-23 00:37:44 +0530

    update

---
+++ my-app/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='container-fluid'>
+        <div className='row'>
+<div className=''>
+
+</div>
+        </div>
+
+    </div>
+  )
+}
