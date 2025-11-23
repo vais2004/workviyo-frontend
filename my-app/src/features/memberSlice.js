002ff3677a02a7cffe51322b7ab13ab98b7c3dc2
vais2004 <vkawale2004@gmail.com>
2025-11-23 23:23:17 +0530

    update

--- my-app/src/features/memberSlice.js
+++ my-app/src/features/memberSlice.js
@@ -1,8 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-//import React, { act } from "react";
-
 export const fetchMembersAsync = createAsyncThunk(
   "members/fetchMembersAsync",
   async () => {
--- my-app/src/features/projectSlice.js
+++ my-app/src/features/projectSlice.js
@@ -1,1 +1,83 @@
-//https://workviyo.vercel.app/
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchProjectsAsync = createAsyncThunk(
+  "projects/fetchProjectsAsync",
+  async ({ projectStatus }) => {
+    const queryParams = new URLSearchParams();
+    const token = localStorage.getItem("token");
+
+    if (projectStatus) queryParams.append("status", projectStatus || "");
+    const response = await axios.get(
+      `https://workviyo.vercel.app/projects?${queryParams.toString()}`,
+      {
+        headers: {
+          Authorization: `${token}`,
+        },
+      }
+    );
+    const data = response.data.project;
+    return data;
+  }
+);
+
+export const addProjectAsync = createAsyncThunk(
+  "projects/addProjectAsync",
+  async ({ name, description, status }) => {
+    const token = localStorage.getItem("token");
+    const response = await axios.post(
+      `https://workviyo.vercel.app/projects`,
+      { name, description, status },
+      {
+        headers: {
+          Authorization: `${token}`,
+        },
+      }
+    );
+    const data = response.data;
+    console.log(response);
+    return data;
+  }
+);
+
+export const updateProjectAsync = createAsyncThunk(
+  "project/updateProjectAsync",
+  async ({ id, name, description, status }) => {
+    try {
+      const token = localStorage.getItem("token");
+      const response = await axios.put(
+        `https://workviyo.vercel.app/projects/${id}`,
+        { name, description, status },
+        { headers: { Authorization: `${token}` } }
+      );
+
+      const data = response.data;
+      console.log(data, "updated project data");
+      return data;
+    } catch (error) {
+      console.log("Error: ", error);
+    }
+  }
+);
+
+export const deleteProjectAsync = createAsyncThunk(
+  "project/deleteProjectAsync",
+  async ({ id }) => {
+    try {
+      const token = localStorage.getItem("token");
+      const response = await axios.delete(
+        `https://workviyo.vercel.app/projects/${id}`,
+        {
+          headers: {
+            Authorization: `${token}`,
+          },
+        }
+      );
+      const data = response.data;
+      console.log(data, "deleted Project data");
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
