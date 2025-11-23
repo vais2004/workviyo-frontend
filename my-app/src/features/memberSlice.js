8f3dda2d776654951443f161cb740b071859b21f
vais2004 <vkawale2004@gmail.com>
2025-11-23 21:04:55 +0530

    update feat: member Slice.js

--- my-app/src/features/memberSlice.js
+++ my-app/src/features/memberSlice.js
@@ -1,1 +1,101 @@
-//https://workviyo.vercel.app/
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+//import React, { act } from "react";
+
+export const fetchMembersAsync = createAsyncThunk(
+  "members/fetchMembersAsync",
+  async () => {
+    const token = localStorage.getItem("token");
+    const response = await axios.get(`https://workviyo.vercel.app/members`, {
+      headers: { Authorization: `${token}` },
+    });
+    const data = response.data;
+    return data;
+  }
+);
+
+export const addMembersAsync = createAsyncThunk(
+  "members/addMembersAsync",
+  async ({ name }) => {
+    const token = localStorage.getItem("token");
+    const response = await axios.post(
+      `https://workviyo.vercel.app/members`,
+      { name },
+      { headers: { Authorization: `${token}` } }
+    );
+    const data = response.data;
+    console.log(data, "data submited");
+    return data;
+  }
+);
+
+export const deleteMembersAsync = createAsyncThunk(
+  "members/deleteMembersAsync",
+  async ({ id }) => {
+    const token = localStorage.getItem("token");
+    const response = await axios.delete(
+      `https://workviyo.vercel.app/members/${id}`,
+      {
+        headers: {
+          Authorization: `${token}`,
+        },
+      }
+    );
+    const data = response.data;
+    console.log(data, "deleted");
+    return data;
+  }
+);
+
+export const memberSlice = createSlice({
+  name: "members",
+  initialState: {
+    members: [],
+    memberStatus: "idle",
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    //fetch
+    builder.addCase(fetchMembersAsync.pending, (state) => {
+      state.memberStatus = "Loading";
+    });
+    builder.addCase(fetchMembersAsync.fulfilled, (state, action) => {
+      state.memberStatus = "All members";
+      state.members = action.payload;
+    });
+    builder.addCase(fetchMembersAsync.rejected, (state, action) => {
+      state.memberStatus = "error";
+      state.error = action.error.message;
+    });
+    //add
+    builder.addCase(addMembersAsync.pending, (state) => {
+      state.memberStatus = "Loading";
+    });
+    builder.addCase(addMembersAsync.fulfilled, (state, action) => {
+      state.memberStatus = "Added members";
+      state.members = action.payload;
+      console.log(action.payload, "payload");
+    });
+    builder.addCase(addMembersAsync.rejected, (state, action) => {
+      state.memberStatus = "error";
+      state.error = action.error.message;
+    });
+    //delete
+    builder.addCase(deleteMembersAsync.pending, (state) => {
+      state.memberStatus = "Loading";
+    });
+    builder.addCase(deleteMembersAsync.fulfilled, (state, action) => {
+      state.memberStatus = "Deleted member";
+      state.members = action.payload;
+      console.log(action.payload, "payload");
+    });
+    builder.addCase(deleteMembersAsync.rejected, (state, action) => {
+      state.memberStatus = "error";
+      state.error = action.error.message;
+    });
+  },
+});
+
+export default memberSlice.reducer;
