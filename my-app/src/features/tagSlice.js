f993a5a37596d9411f2de2006bc9d7c220812d11
vais2004 <vkawale2004@gmail.com>
2025-11-24 20:28:02 +0530

    update feat: tagSlice.js

--- my-app/src/features/tagSlice.js
+++ my-app/src/features/tagSlice.js
@@ -1,1 +1,68 @@
-//https://workviyo.vercel.app/
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchTagsAsync = createAsyncThunk(
+  "tags/fetchTagsAsync",
+  async () => {
+    const token = localStorage.getItem("token");
+    const response = await axios.get(`https://workviyo.vercel.app/tags`, {
+      headers: { Authorization: `${token}` },
+    });
+    const data = response.data;
+    return data;
+  }
+);
+
+export const addTagsAsync = createAsyncThunk(
+  "tags/addTagsAsync",
+  async (newTag) => {
+    const token = localStorage.getItem("token");
+    const response = await axios.post(
+      `https://workviyo.vercel.app/tags`,
+      newTag,
+      { headers: { Authorization: `${token}` } }
+    );
+    const data = response.data;
+    console.log(data, "data submit");
+    return data;
+  }
+);
+
+export const tagSlice = createSlice({
+  name: "tags",
+  initialState: {
+    tags: [],
+    tagStatus: "idle",
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchTagsAsync.pending, (state) => {
+      state.tagStatus = "Loading";
+    });
+    builder.addCase(fetchTagsAsync.fulfilled, (state, action) => {
+      state.tagStatus = "All tags";
+      state.tags = action.payload;
+      console.log(action.payload, "payload");
+    });
+    builder.addCase(fetchTagsAsync.rejected, (state, action) => {
+      state.tagStatus = "error";
+      state.error = action.error.message;
+    });
+
+    //add tags
+    builder.addCase(addTagsAsync.pending, (state) => {
+      state.tagStatus = "Loading";
+    });
+    builder.addCase(addTagsAsync.fulfilled, (state, action) => {
+      state.tagStatus = "Added tags";
+      state.tags = action.payload;
+      console.log(action.payload, "payload");
+    });
+    builder.addCase(addTagsAsync.rejected, (state, action) => {
+      state.tagStatus = "error";
+      state.error = action.error.message;
+    });
+  },
+});
+export default tagSlice.reducer;
