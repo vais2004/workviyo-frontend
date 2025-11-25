c2f330b517009f448a80a75d3fd380795e70d458
vais2004 <vkawale2004@gmail.com>
2025-11-25 20:47:19 +0530

    update

--- my-app/src/features/userSlice.js
+++ my-app/src/features/userSlice.js
@@ -1,1 +1,47 @@
-//https://workviyo.vercel.app/
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchUserAsync = createAsyncThunk(
+  "users/fetchUserAsync",
+  async () => {
+    const response = await axios.get(`https://workviyo.vercel.app/users`, {
+      headers: {
+        Authorization: `${token}`,
+      },
+    });
+    const data = response.data;
+    return data;
+  }
+);
+
+export const registerUserAsync = createAsyncThunk(
+  "users/registerUserAsync",
+  async (credentials) => {
+    const response = await axios.post(
+      `https://workviyo.vercel.app/users/register`,
+      credentials
+    );
+    const data = response.data;
+    console.log("signup response:", response.data);
+    localStorage.setItem("token", response.data.token);
+    console.log(data, "data submit");
+    return data;
+  }
+);
+
+export const userLoginAsync = createAsyncThunk(
+  "users/userLoginAsync",
+  async (credentials) => {
+    try {
+      const response = await axios.post(
+        `https://workviyo.vercel.app/users/login`,
+        credentials
+      );
+      console.log("Login response: ", response.data);
+      localStorage.setItem("token", response.data.token);
+      return response.data;
+    } catch (error) {
+      console.error("Login error: ", error.response?.data);
+    }
+  }
+);
