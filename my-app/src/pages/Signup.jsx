1d93742fc2c848344c1593a8e7e1665f6208b78c
vais2004 <vkawale2004@gmail.com>
2025-12-01 17:30:49 +0530

    update feat: wrote page of signing up or continue as guest

---
+++ my-app/src/pages/Signup.jsx
@@ -0,0 +1,100 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { registerUserAsync, userLoginAsync } from "../features/userSlice";
+import ShowHidePassword from "../components/ShowHidePassword";
+
+export default function Signup() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleUserRegister = async (event) => {
+    event.preventDefault();
+    try {
+      await dispatch(registerUserAsync({ name, email, password })).unwrap();
+      toast.success("Sighup successful!");
+      navigate("/dashboard");
+    } catch (error) {
+      toast.error("Invalid credentials!");
+    }
+  };
+
+  const guestLoginHandler = async () => {
+    setEmail("vkawale2004@gmail.com");
+    setPassword("vkawale2004");
+
+    try {
+      await dispatch(userLoginAsync({ email, password })).unwrap();
+      toast.success("Login successful!");
+      navigate("/dashboard");
+    } catch (error) {
+      toast.error("Invalid credentials while Logging In!");
+    }
+  };
+
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      <div
+        className="card p-4 border-0"
+        style={{ width: "400px", backgroundColor: "#ffffff" }}>
+        <h4 className="heading-color text-center mb-3">Workviyo</h4>
+        <h2 className="text-center">Create an account</h2>
+        <p className="text-center text-muted mb-4">Please enter your details</p>
+        <form onSubmit={handleUserRegister}>
+          <label htmlFor="name" className="form-label fw-semibold mb-1">
+            Name
+          </label>
+          <input
+            type="text"
+            placeholder="Enter your Name"
+            value={name}
+            className="form-control"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <label htmlFor="email" className="form-label fw-semibold mb-1">
+            Email
+          </label>
+          <input
+            type="email"
+            placeholder="Enter your Email"
+            value={email}
+            className="form-control"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label
+            htmlFor="password"
+            className="form-label fw-semibold mb-1 mt-3">
+            Password
+          </label>
+          <ShowHidePassword
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <div className="d-grid my-2">
+            <button className="btn btn-outline-primary" type="submit">
+              Sign In
+            </button>
+          </div>
+          <div className="d-grid mb-2">
+            <button
+              className="btn btn-outline-secondary"
+              onClick={() => guestLoginHandler}>
+              Continue as Guest
+            </button>
+          </div>
+          <p>
+            Already have an account?{" "}
+            <Link to="/" className="text-decoration-none fw-semibold ">
+              Log In
+            </Link>{" "}
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+}
