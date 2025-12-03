c405a74d1bf0f1d7999ea22fb02bacce3ce4a4d4
vais2004 <vkawale2004@gmail.com>
2025-12-03 19:13:24 +0530

    update feat: App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,36 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home.jsx";
+import Projects from "./pages/Projects.jsx";
+import Teams from "./pages/Teams.jsx";
+import TeamDetail from "./pages/TeamDetail.jsx";
+import Reports from "./pages/Report/Reports.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+import Settings from "./pages/Settings.jsx";
+import AddTask from "./pages/AddTask.jsx";
+import AddProject from "./Pages/AddProject.jsx";
+import AddTeam from "./pages/AddTeam.jsx";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route/>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/projects/:projectId" element={<Projects />} />
+        <Route path="/reports" element={<Reports />} />
+        <Route path="/teams" element={<Teams />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/teamDetail/:teamId" element={<TeamDetail />} />
+        <Route path="/addTask" element={<AddTask />} />
+        <Route path="/addTask/:taskId" element={<AddTask />} />
+        <Route path="/addProject" element={<AddProject />} />
+        <Route path="/addProject/:projectId" element={<AddProject />} />
+        <Route path="/addTeam" element={<AddTeam />} />
+        <Route path="/addTeam/:teamId" element={<AddTeam />} />
       </Routes>
     </>
   );
