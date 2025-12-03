772b7ff01afb0868f51fc1439814894c665f1f49
vais2004 <vkawale2004@gmail.com>
2025-12-03 18:51:09 +0530

    update feat: reports.jsx page

---
+++ my-app/src/pages/Report/Reports.jsx
@@ -0,0 +1,64 @@
+import SideNav from "../../components/SideNav";
+import { TotalWorkDoneLastWeek } from "./TotalWorkDoneLastWeek";
+import TaskClosedBy from "./TaskClosedBy";
+import TotalDaysOfWorkPending from "./TotalDaysOfWorkPending";
+
+export default function Reports() {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div
+          className="offcanvas offcanvas-start"
+          tabIndex="-1"
+          id="mobileSidebar"
+          aria-labelledby="mobileSidebarLabel"
+          style={{ width: "250px" }}>
+          <div className="offcanvas-header">
+            <button
+              type="button"
+              className="btn-close text-reset"
+              data-bs-dismiss="offcanvas"
+              aria-label="Close"></button>
+          </div>
+          <div className="offcanvas-body p-0">
+            <SideNav />
+          </div>
+        </div>
+
+        <div
+          className="col-12 col-md-3 col-lg-2 d-none d-md-block p-0"
+          style={{
+            position: "sticky",
+            top: 0,
+            height: "100vh",
+            overflowY: "auto",
+          }}>
+          <SideNav />
+        </div>
+
+        <div className="col-12 col-md-9 col-lg-10 p-4">
+          <button
+            className="btn btn-outline-primary d-md-none mb-3"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#mobileSidebar"
+            aria-controls="mobileSidebar">
+            <i className="bi bi-list"></i>
+          </button>
+          <section className="pb-3 px-2">
+            <span className="heading-color fw-bold fs-2">Reports</span>
+          </section>
+          <section className="p-3 shadow mb-4">
+            <TotalWorkDoneLastWeek />
+          </section>
+          <section className="p-3 shadow mb-4">
+            <TotalDaysOfWorkPending />
+          </section>
+          <section className="p-3 shadow mb-4">
+            <TaskClosedBy />
+          </section>
+        </div>
+      </div>
+    </div>
+  );
+}
