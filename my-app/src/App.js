20818b19521be3e06fe64545f95e8d5e72138a1d
vais2004 <vkawale2004@gmail.com>
2025-12-19 21:35:40 +0530

    update polishing and correcting the routes and removing commented code from the App.js page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -16,37 +16,21 @@ function App() {
   return (
     <>
       <Routes>
-        {/* <Route path="/" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
-        <Route path="/projects/:projectId" element={<Projects />} />
-        <Route path="/reports" element={<Reports />} />
+        <Route path="/projects/:projectId" element={<AddProject />} />
+        <Route path="/addProject" element={<AddProject />} />
         <Route path="/teams" element={<Teams />} />
-        <Route path="/settings" element={<Settings />} />
         <Route path="/teamDetail/:teamId" element={<TeamDetail />} />
         <Route path="/addTask" element={<AddTask />} />
         <Route path="/addTask/:taskId" element={<AddTask />} />
-        <Route path="/addProject" element={<AddProject />} />
-        <Route path="/addProject/:projectId" element={<AddProject />} />
+        <Route path="/reports" element={<Reports />} />
+        <Route path="/settings" element={<Settings />} />
         <Route path="/addTeam" element={<AddTeam />} />
         <Route path="/addTeam/:teamId" element={<AddTeam />} />
-      */}
-        <Route path="/" element={<Login />} />
-  <Route path="/signup" element={<Signup />} />
-  <Route path="/dashboard" element={<Home />} />
-  <Route path="/projects" element={<Projects />} />               {/* List all projects */}
-  <Route path="/projects/view/:projectId" element={<AddProject />} /> {/* View/Edit project */}
-  <Route path="/addProject" element={<AddProject />} />          {/* Create new project */}
-  <Route path="/teams" element={<Teams />} />
-  <Route path="/teamDetail/:teamId" element={<TeamDetail />} />
-  <Route path="/addTask" element={<AddTask />} />                {/* Create new task */}
-  <Route path="/addTask/:taskId" element={<AddTask />} />       {/* Edit existing task */}
-  <Route path="/reports" element={<Reports />} />
-  <Route path="/settings" element={<Settings />} />
-  <Route path="/addTeam" element={<AddTeam />} />
-  <Route path="/addTeam/:teamId" element={<AddTeam />} />
-      </Routes> 
+      </Routes>
     </>
   );
 }
