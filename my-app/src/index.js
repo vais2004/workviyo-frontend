f5e44725cfefe5ef7801c7fc2d2e1bef6c1e3f6b
vais2004 <vkawale2004@gmail.com>
2026-01-11 17:54:10 +0530

    update

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import "react-toastify/dist/ReactToastify.css";
 import store from "./app/store.js";
 import App from "./App.js";
 
